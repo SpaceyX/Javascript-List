01be18047fe2b80877f6f8f93fb6d1e3acbe7c5a
SpaceyX <stenderd@gmail.com>
2018-01-16 11:16:42 +0100

    x

--- List.js
+++ List.js
@@ -1,12 +1,15 @@
 class List {
+    //initializes as new List-object.
     constructor() {
         this.source = [];
     }
 
+    //appends and item.
     add(object) {
         this.source.push(object);
     }
 
+    //removes a specific item by reference.
     remove(object) {
         for(let i = this.source.length; i > -1; i--) {
             if(object === this.source[i]) {
@@ -16,16 +19,19 @@ class List {
         }
     }
 
+    //removes an item at a spcific indes.
     removeAt(index) {
         this.source.splice(index, 1);
     }
 
+    //returns the first item and deletes it from the underlying array.
     pop() {
         let x = this.source[0];
         this.removeAt(0);
         return x;
     }
 
+    //inserts an item at a specific index.
     insert(index, object) {
         let x = [];
         let off = 0;
@@ -40,6 +46,12 @@ class List {
         this.source = x;
     }
 
+    //clears the underlying array.
+    clear() {
+        this.source = [];
+    }
+
+    //adds an item at index 0 to the underlying array.
     push(object) {
         let x = [];
         x[0] = object;
@@ -49,19 +61,34 @@ class List {
         this.source = x;
     }
 
+    //returns the item at index 0 without removing it from the underlying array.
     peek() {
         return this.source[0];
     }
 
+    //returns an item at a specific index.
     get(index) {
         return this.source[index];
     }
 
+    //returns the first item. analog to peek().
     getFirst() {
         return this.peek();
     }
 
+    //returns the last item.
     getLast() {
         return this.source[this.source.length -1];
+    }
+
+    //reverses the underlying array.
+    reverse() {
+        let x = [];
+        let y = 0;
+        for(let i = this.source.length - 1; i > -1; i--) {
+            x[y] = this.source[i];
+            y++;
+        }
+        this.source = x;
     }
 }
